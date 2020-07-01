feedabb9e6abac7e13baccb7da74639395f18693
Julien Coppenolle <58251569+K0b4l@users.noreply.github.com>
2020-07-01 14:43:29 +0200

    Added Scripts Needed for Canons to work
    added
    CanonsBehaviour Script (allows follow target and shoot)
    MissileBehaviour (allows missiles to move once spawned)
    Spawner (allows to spawn object in certain circunstances)

---
+++ Scripts/CanonsBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonsBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    Transform _target;
+    public Spawner[] _cannonsTip;
+    public MissileBehaviour _projectilePrefab;
+
+    Vector3 _targetDirection;
+    Vector3 _localLookVector = Vector3.up;
+    Vector3 _rotationAxis = Vector3.forward;
+    Vector3 _worldLookAxis;
+
+    float _delta;
+    float _rotationStep;
+
+    public float _lookTowardSpeed;
+
+    [Range(.1f,10f)]
+    public float _shootReload = 5f;
+
+    private void Awake()
+    {
+        CountdownShoot();
+    }
+
+    private void Update()
+    {
+        _worldLookAxis = transform.TransformDirection(_localLookVector).normalized;
+        _targetDirection = (_target.position - transform.position).normalized;
+
+        //if (Mathf.Abs(_delta) == 180f)
+        if (Vector3.Dot(_worldLookAxis, _targetDirection) == -1) // might want to change for previous condition
+        {
+            _worldLookAxis = (_worldLookAxis + transform.right * 0.01f);
+        }
+
+        _rotationAxis = Vector3.Cross(_targetDirection, _worldLookAxis);
+
+        _delta = Vector3.SignedAngle(_worldLookAxis, _targetDirection, _rotationAxis);
+        _rotationStep = Mathf.Min(Mathf.Abs(_delta), _lookTowardSpeed * Time.deltaTime) * Mathf.Sign(_delta);
+
+        transform.Rotate(_rotationAxis, _rotationStep, Space.World);
+    }
+
+    void CountdownShoot()
+    {
+        Invoke("Shoot",_shootReload);
+    }
+
+    void Shoot()
+    {
+        for (int i = 0; i < _cannonsTip.Length; i++)
+        {
+            GameObject tempSpawn = _cannonsTip[i].Spawn(_projectilePrefab.gameObject);
+        }
+        Invoke("CountdownShoot",0);
+    }
+}
---
+++ Scripts/Spawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Spawn(GameObject prefab)
+    {
+        return Instantiate(prefab, transform.position, transform.rotation);
+    }
+}
