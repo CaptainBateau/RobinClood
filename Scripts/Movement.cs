122e0b27f62a3b7df4c88fcef0e48f6981322efb
CaptainBateau <matteohx@gmail.com>
2020-07-01 11:32:50 +0200

    First Push

---
+++ Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float _horizontalSpeed;
+	public float _verticalSpeed;
+
+	void Update()
+	{
+		
+			transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * _horizontalSpeed * Time.deltaTime);
+		
+
+		
+			transform.Translate(Input.GetAxis("Vertical") * Vector3.up * _verticalSpeed * Time.deltaTime);
+		
+	}
+}
